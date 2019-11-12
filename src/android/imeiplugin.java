77bb74ab3cec476fc8d7d790ddc498c58426bd70
Thop <14835863+thop7@users.noreply.github.com>
2019-11-12 17:08:09 +0100

    Update imeiplugin.java
    Add control and request permission for READ_PHONE_STATE.

--- src/android/imeiplugin.java
+++ src/android/imeiplugin.java
@@ -1,7 +1,9 @@
 package com.joandilee;
 
+import android.Manifest;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PermissionHelper;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -12,18 +14,24 @@ import android.content.Context;
 import android.util.Log;
 
 public class imeiplugin extends CordovaPlugin {
+    private CallbackContext mCallbackContext;
+
 	@Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        mCallbackContext = callbackContext;
         if (action.equals("getImei")) {
-            this.DeviceImeiNumber(callbackContext);
+            if(PermissionHelper.hasPermission(this, Manifest.permission.READ_PHONE_STATE)) {
+                this.DeviceImeiNumber(callbackContext);
+            } else {
+                PermissionHelper.requestPermission(this, 0, Manifest.permission.READ_PHONE_STATE);
+            }
             return true;
         }
         return false;
     }
 
     public void DeviceImeiNumber(CallbackContext callbackContext) {
-        Context context=this.cordova.getActivity().getApplicationContext();
-
+        Context context = this.cordova.getActivity().getApplicationContext();
         TelephonyManager tManager = (TelephonyManager)cordova.getActivity().getSystemService(context.TELEPHONY_SERVICE);
         callbackContext.success(tManager.getDeviceId());
     }
@@ -33,6 +41,15 @@ public class imeiplugin extends CordovaPlugin {
             callbackContext.success(message);
         } else {
             callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+    public void onRequestPermissionResult(int requestCode, String[] permissions,
+                                          int[] grantResults) throws JSONException
+    {
+
+        if(requestCode == 0) {
+            this.DeviceImeiNumber(mCallbackContext);
         }
     } 
 } 
